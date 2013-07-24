516c3ed514f9b1ecd8abd7f325c19ed925bb6c0c
Dan Delany <dan.delany@gmail.com>
2013-07-24 19:50:57 -0400

    right justify bikes text

--- biketerm.py
+++ biketerm.py
@@ -1,11 +1,9 @@
 from __future__ import print_function
 import urllib2, json, os, time, sys
 
-#FAVORITE_STATIONS = []
+FAVORITE_STATIONS = []
 #FAVORITE_STATIONS = [250, 251, 303, 317, 326, 394, 432, 445]
-FAVORITE_STATIONS = [150, 250, 251, 266, 301, 303, 317, 326, 339, 375, 393, 394, 432, 433, 445]
-
-
+#FAVORITE_STATIONS = [150, 250, 251, 266, 301, 303, 317, 326, 339, 375, 393, 394, 432, 433, 445]
 
 def colored(text, color):
     colors = {'red': '\033[91m', 'green': '\033[92m'}
@@ -19,10 +17,7 @@ def print_header(data_time):
     print('CitiBike station data as of', data_time)
     if not len(FAVORITE_STATIONS):
         print('Favorite stations not set, showing all stations')
-    print('LEGEND: ', end='')
-    print(colored("| broken bike", 'red'), end='  ')
-    print(colored("| available bike", 'green'), end='  ')
-    print(": empty dock")
+    print('LEGEND: ', colored("| broken bike ", 'red'), colored("| available bike ", 'green'), ": empty dock")
 
 def main():
     try:
@@ -31,12 +26,13 @@ def main():
         data_time = stations_data['executionTime']
         stations = stations_data['stationBeanList']
         my_stations = filter(lambda station: station['id'] in FAVORITE_STATIONS or not len(FAVORITE_STATIONS), stations)
-        
+        max_docks = reduce(lambda max_docks, station: max(max_docks, station['totalDocks']), my_stations, 0)
+        print(max_docks)
         print_header(data_time)
         
         for station in my_stations:
             if station['id'] in FAVORITE_STATIONS or not len(FAVORITE_STATIONS):
-                station_name_trimmed = station['stationName'][0:22].ljust(22)
+                station_name_trimmed = station['stationName'][0:22].ljust(25)
                 print(station['id'], '\t', station_name_trimmed, end='  ')
                 
                 if(station['statusKey'] == 1):
@@ -49,7 +45,8 @@ def main():
                     print(colored(available_bikes * "|", 'green'), end='')
                     print(available_docks * ":", end='   ')
                     
-                    bikes_str_trimmed = (str(available_bikes) + '/' + str(total_docks) + ' bikes ')
+                    rjust_spaces = max_docks - total_docks
+                    bikes_str_trimmed = ((" " * rjust_spaces) + str(available_bikes) + '/' + str(total_docks) + ' bikes ')
                     print(bikes_str_trimmed)
                     #print('(', station['availableBikes'], '/', station['totalDocks'], 'bikes)')
                 else:
